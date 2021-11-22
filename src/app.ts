1d8e83d215ce78fd8e21d3c903f0e292a1243983
Simone Ballard <simone.a.ballard@gmail.com>
2021-11-21 20:23:17 -0500

    added google geocoding response type

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,21 @@ const addressInput = document.getElementById('address')! as HTMLInputElement;
 
 const GOOGLE_API_KEY = 'AIzaSyAg04SEEvjKzd7HKphLpO5btS7rZj97eZE'
 
+type GoogleGeocodingResponse = {
+    results: {geometry: {location: {lat: number, lng: number}}}[];
+};
+
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
 
-    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+    axios
+        .get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
     )}&key=${GOOGLE_API_KEY}`
     )
     .then(response => {
-        console.log(response);
+        const coordinates = response.data.results[0].geometry.location;
     })
     .catch(err => {
         console.log(err);
