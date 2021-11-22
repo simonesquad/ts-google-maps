d1baf735c746406eaf2e39e7ae7326ff135a34b0
Simone Ballard <simone.a.ballard@gmail.com>
2021-11-21 20:16:26 -0500

    received one response to the http request

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,24 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyAg04SEEvjKzd7HKphLpO5btS7rZj97eZE'
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
 
-    // send the address to Google's API
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+    )}&key=${GOOGLE_API_KEY}`
+    )
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 }
 
 form.addEventListener('submit', searchAddressHandler);
